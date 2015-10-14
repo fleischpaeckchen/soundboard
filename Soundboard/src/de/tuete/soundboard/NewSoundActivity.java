6cba7b2bce9e9e3942dbbcccd37b85d23a019653
Johannes Riegner <jriegner@live.de>
2015-10-14 22:11:33 +0200

    - added NewSoundActivity which shall receive the .aac files of our whatsapp conversations
    It is fucking unstable, nothing seems to work. Seems to be a more
    complicated topic.

---
+++ Soundboard/src/de/tuete/soundboard/NewSoundActivity.java
@@ -0,0 +1,117 @@
+package de.tuete.soundboard;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import de.tuete.soundboard.adapter.AtzenSpinnerAdapter;
+import de.tuete.soundboard.model.Atze;
+
+public class NewSoundActivity extends Activity {
+	
+	private Atze[] atzen;
+	private EditText txt_soundname;
+	private Spinner sp_atzen;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_new_sound);
+		
+		Intent intent = getIntent();
+		String action = intent.getAction();
+		String type = intent.getType();
+		
+		createAtzenArray();
+		this.txt_soundname = (EditText) findViewById(R.id.txt_sound_name);
+		this.sp_atzen = (Spinner) findViewById(R.id.sp_atzen);
+		
+		AtzenSpinnerAdapter adapter = new AtzenSpinnerAdapter(this, R.layout.spinner_row_atzen, atzen);
+		this.sp_atzen.setAdapter(adapter);
+		
+		if(Intent.ACTION_SEND.equals(action) && type != null){
+			if("audio/*".equals(type)){
+				//handleAudio(intent);
+			}
+		}
+	}
+	
+	public void saveSound(View view){
+		
+	}
+	
+	private void handleAudio(Intent intent){
+		Uri uri = (Uri)intent.getParcelableExtra(Intent.EXTRA_STREAM);
+		File src = new File(uri.getPath());
+		File dst = new File(getFilesDir().getPath() + "test.aac");
+		
+		InputStream is;
+		try {
+			is = new FileInputStream(src);
+			OutputStream os = new FileOutputStream(dst);
+			
+	        byte[] buff=new byte[1024];
+	        int len;
+	        while((len = is.read(buff)) > 0){
+	            os.write(buff,0,len);
+	        }
+	        is.close();
+	        os.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+		MediaPlayer player = MediaPlayer.create(this, Uri.fromFile(dst));
+		player.start();
+	}
+	
+	private void createAtzenArray(){
+		Atze jonas = new Atze("Jonas", R.drawable.jonas);
+		Atze nobi = new Atze("Nobi", R.drawable.nobke);
+		Atze schlotte = new Atze("Schlotte", R.drawable.schlotte);
+		
+		this.atzen = new Atze[3];
+		atzen[0] = jonas;
+		atzen[1] = nobi;
+		atzen[2] = schlotte;
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.new_sound, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
---
+++ Soundboard/src/de/tuete/soundboard/adapter/AtzenSpinnerAdapter.java
@@ -0,0 +1,48 @@
+package de.tuete.soundboard.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import de.tuete.soundboard.R;
+import de.tuete.soundboard.model.Atze;
+
+public class AtzenSpinnerAdapter extends ArrayAdapter<Atze> {
+	
+	private Context context;
+	private Atze[] atzen;
+
+	public AtzenSpinnerAdapter(Context context, int resource, Atze[] objects) {
+		super(context, resource, objects);
+		this.context = context;
+		this.atzen = objects;
+	}
+
+	@Override
+	public View getDropDownView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		return getCustomView(position, convertView, parent);
+	}
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		return getCustomView(position, convertView, parent);
+	}
+	
+	public View getCustomView(int position, View convertView, ViewGroup parent){
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View row = inflater.inflate(R.layout.spinner_row_atzen, parent, false);
+		
+		ImageView img_atze = (ImageView) row.findViewById(R.id.img_atze);
+		img_atze.setImageResource(atzen[position].getImg_resource());
+		
+		TextView txt_atze_name = (TextView) row.findViewById(R.id.txt_atze_name);
+		txt_atze_name.setText(atzen[position].getName());
+		
+		return row;
+	}
+}
